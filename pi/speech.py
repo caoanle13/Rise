209d288da8adaa8ed181417c5eaf63b57138e50e
Cao An <caoan.le15@imperial.ac.uk>
2019-02-07 15:50:18 +0000

    added speech output communication to PI via TIME_SET message

--- pi/speech.py
+++ pi/speech.py
@@ -7,10 +7,15 @@ nlp = NLP()
 
 # Constants
 SPEECH_TRIGGER = 0
+TIME_SET = 1
+SUNRISE = 0
+AT = 1
+TO_PI = "IC.embedded/tEEEm/TO_PI"
+TO_APP = "IC.embedded/tEEEm/TO_APP"
 
 client = mqtt.Client()
 client.connect("test.mosquitto.org",port=1883)
-client.subscribe("IC.embedded/tEEEm/TO_PI")
+client.subscribe(TO_PI)
 
 def recognize():
     r = sr.Recognizer()
@@ -39,9 +44,12 @@ def on_message(client, userdata, message):
             print("output of speech recognition:", text)
             nlp_success, meaning = nlp.parse(text)
             if nlp_success:
-                print("info from NLP:", meaning)
-                
-
+                print("output of natural language processing:", meaning)
+                if meaning == 'sunrise':
+                    data = json.dumps({'type': TIME_SET, 'nature': SUNRISE})
+                else:
+                    data = json.dumps({'type': TIME_SET, 'nature': AT, 'time': meaning})
+                client.publish(TO_PI, data)
 
 client.on_message = on_message
 client.loop_forever()
