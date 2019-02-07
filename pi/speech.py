2d729ea0cff5fbbe41c64a4599d0f79378c741f5
Cao An <caoan.le15@imperial.ac.uk>
2019-02-07 13:18:04 +0000

    added speech recognition, string output

--- pi/speech.py
+++ pi/speech.py
@@ -1,5 +1,7 @@
 import paho.mqtt.client as mqtt
 import json
+import speech_recognition as sr
+
 
 # Constants
 SPEECH_TRIGGER = 0
@@ -9,12 +11,26 @@ client.connect("test.mosquitto.org",port=1883)
 client.subscribe("IC.embedded/tEEEm/TO_PI")
 
 def on_message(client, userdata, message):
-    print('received message')
+    #print('received message')
     message_payload = json.loads(message.payload.decode())
     message_type = message_payload['type']
     
     if message_type == SPEECH_TRIGGER:
-        print('this is a speech trigger from the web page')
+        #print('this is a speech trigger from the web page')
+
+        r = sr.Recognizer()
+        print('Speech starts listening')
+        with sr.Microphone() as source:
+            print("Say something!")
+            audio = r.listen(source)
+            try:
+                print("You said " + r.recognize_google(audio))
+            except sr.UnknownValueError:
+                print("Not understood")
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+
 
 client.on_message = on_message
 client.loop_forever()
