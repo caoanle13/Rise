e96daecafcb6904f1e7d3e35d8581a71642ef6ca
Cao An <caoan.le15@imperial.ac.uk>
2019-02-07 16:31:31 +0000

    added skeleton for all messages to and from the App

--- www/index.js
+++ www/index.js
@@ -2,11 +2,15 @@
 const SPEECH_TRIGGER = 0;
 const TIME_SET = 1;
 const ASK_RESULTS = 2;
-// TO_PI TOPIC MESSAGES
+// TO_APP TOPIC MESSAGES
 const START_ALARM = 0;
 const STOP_ALARM = 1;
-const GIVE_RESULTS = 2;
+const RESULTS = 2;
 
+var audio = new Audio('alarm.mp3');
+var temperatureData;
+
+//var audio = document.getElementById('alarm')
 
 var client = mqtt.connect('mqtt://test.mosquitto.org:8080');
 
@@ -19,6 +23,31 @@ client.on('connect', function () {
     });
 });
 
+client.on('message', function (topic, message) {
+    //console.log(message)
+    //console.log('received message!');
+    var message = JSON.parse(message)
+    if(topic === 'IC.embedded/tEEEm/TO_APP'){
+        if (message.type == START_ALARM){
+            console.log('play here')
+            //audio.play();
+        }
+        else if (message.type == STOP_ALARM){
+            //audio.pause();
+            console.log('pause audio');
+        }
+        else if (message.type == RESULTS){
+            temperatureData = message.data;
+        }
+    }
+});
+
 function sendSleepTriggerMessage() {
     client.publish('IC.embedded/tEEEm/TO_PI', JSON.stringify({'type': SPEECH_TRIGGER})); 
+}
+
+function showTemperatureData(){
+    console.log('show temperature data here');
+    client.publish('IC.embedded/tEEEm/TO_PI', JSON.stringify({'type': ASK_RESULTS}));
+    // use temperatureData with a graph library to create a chart.
 }
