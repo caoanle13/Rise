ad24eb28b677bde2cae72115f5c6d2b176837142
Becca Hallam <rch16@ic.ac.uk>
2019-02-11 22:17:47 +0000

    tested and fixed a few errors. TO DO: replace sleep with threading.

--- pi/led.py
+++ pi/led.py
@@ -7,8 +7,8 @@ from time import sleep
 
 class LED:
 
-    def __init__(self, r, g, b):
-        
+    def __init__(self, r, g, b, r_duty, g_duty, b_duty):
+
         # choose BCM or BOARD numbering schemes
         GPIO.setmode(GPIO.BOARD)
 
@@ -16,7 +16,7 @@ class LED:
         GPIO.setwarnings(False)
 
         # setup
-        self.RGB_pins = [12,11,13]
+        self.RGB = [12,11,13]
         for pin in self.RGB:
             # sets each pin as output for LED
             GPIO.setup(pin,GPIO.OUT,initial=GPIO.HIGH)
@@ -27,9 +27,9 @@ class LED:
         self.B = GPIO.PWM(13,b)
 
         # start LED OFF
-        self.R.start(100)
-        self.G.start(100)
-        self.B.start(100)
+        self.R.start(r_duty)
+        self.G.start(g_duty)
+        self.B.start(b_duty)
 
 
     def gradual_on(self, pause_time):
@@ -39,27 +39,27 @@ class LED:
             self.G.ChangeDutyCycle(i)
             self.B.ChangeDutyCycle(i)
             sleep(pause_time)
-    
+
     def turn_on(self):
         self.R.ChangeDutyCycle(0)
         self.G.ChangeDutyCycle(0)
         self.B.ChangeDutyCycle(0)
-    
+
     def turn_off(self):
         self.R.ChangeDutyCycle(100)
         self.G.ChangeDutyCycle(100)
         self.B.ChangeDutyCycle(100)
 
-    def change_colour(self, r, g, b):
-        self.R = GPIO.PWM(12,r)
-        self.G = GPIO.PWM(11,g)
-        self.B = GPIO.PWM(13,b)
+    def change_colour(self, r_duty, g_duty, b_duty):
+        self.R.ChangeDutyCycle(r_duty)
+        self.G.ChangeDutyCycle(g_duty)
+        self.B.ChangeDutyCycle(b_duty)
 
 
 
 if __name__ == "__main__":
 
-    led = LED()
+    led = LED(1000,1000,1000,100,100,100)
     # test gradual turn on
     led.gradual_on(0.08)
     sleep(1)
@@ -70,12 +70,21 @@ if __name__ == "__main__":
 
     # test turn on
     led.turn_on()
+    sleep(1)
 
     # test change colour
-    led.change_colour(1000, 500, 100)
+    led.change_colour(0,100,100)    # red
+    sleep(1)
+    led.change_colour(0,0,100)      # red + green
+    sleep(1)
+    led.change_colour(100,0,100)    # green
+    sleep(1)
+    led.change_colour(100,0,0)      # green + blue
+    sleep(1)
+    led.change_colour(100,100,0)    # blue
+    sleep(1)
+    led.change_colour(0,100,0)      # blue + red
+    sleep(1)
 
-
-
-
-        
-        
+    led.turn_off()
+    GPIO.cleanup()
