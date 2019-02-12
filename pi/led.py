a790c204f55940f619b900c719781fd62856ab8d
Cao An <caoan.le15@imperial.ac.uk>
2019-02-12 16:21:43 +0000

    modified led class to have increment/decrement method

--- pi/led.py
+++ pi/led.py
@@ -9,6 +9,12 @@ class LED:
 
     def __init__(self, r, g, b, r_duty, g_duty, b_duty):
 
+        # intensity of LED
+        self.r_duty = r_duty
+        self.g_duty = g_duty
+        self.b_duty = b_duty
+
+
         # choose BCM or BOARD numbering schemes
         GPIO.setmode(GPIO.BOARD)
 
@@ -26,19 +32,27 @@ class LED:
         self.G = GPIO.PWM(11,g)
         self.B = GPIO.PWM(13,b)
 
-        # start LED OFF
-        self.R.start(r_duty)
-        self.G.start(g_duty)
-        self.B.start(b_duty)
+        # start LED
+        self.R.start(self.r_duty)
+        self.G.start(self.g_duty)
+        self.B.start(self.b_duty)
 
 
-    def gradual_on(self, pause_time):
-        # brighten LED
-        for i in range(100,-1,-1):
-            self.R.ChangeDutyCycle(i)
-            self.G.ChangeDutyCycle(i)
-            self.B.ChangeDutyCycle(i)
-            sleep(pause_time)
+    def increment_LED(self):
+        self.r_duty = self.r_duty - 1 if self.r_duty > 0 else 0
+        self.g_duty = self.g_duty - 1 if self.g_duty > 0 else 0
+        self.b_duty = self.b_duty - 1 if self.b_duty > 0 else 0
+        self.R.ChangeDutyCycle(self.r_duty)
+        self.G.ChangeDutyCycle(self.g_duty)
+        self.B.ChangeDutyCycle(self.b_duty)
+
+    def decrement_LED(self):
+        self.r_duty = self.r_duty + 1 if self.r_duty < 100 else 100
+        self.g_duty = self.g_duty + 1 if self.g_duty < 100 else 100
+        self.b_duty = self.b_duty + 1 if self.b_duty < 100 else 100
+        self.R.ChangeDutyCycle(self.r_duty)
+        self.G.ChangeDutyCycle(self.g_duty)
+        self.B.ChangeDutyCycle(self.b_duty)
 
     def turn_on(self):
         self.R.ChangeDutyCycle(0)
@@ -60,16 +74,13 @@ class LED:
 if __name__ == "__main__":
 
     led = LED(1000,1000,1000,100,100,100)
-    # test gradual turn on
-    led.gradual_on(0.08)
+
+    # test turn on
+    led.turn_on()
     sleep(1)
 
     # test turn off
     led.turn_off()
-    sleep(1)
-
-    # test turn on
-    led.turn_on()
     sleep(1)
 
     # test change colour
