3fc81c09e766505d47dc32785c98d0f410c18dc8
Cao An <caoan.le15@imperial.ac.uk>
2019-02-12 16:26:21 +0000

    added SpeechSynthesis class for sending speech messages to the app

---
+++ pi/speech_synthesis.py
@@ -0,0 +1,15 @@
+from weather import Weather
+
+
+
+class SpeechSynthesis():
+    def __init__(self):
+        self.messages = {
+            'WAKEUP_SUNRISE' : 'Okay I will wake you up when the sun rises',
+            'WAKEUP_TIME' : 'Okay I will set an alarm for ',
+            'GOOD_MORNING': 'Good Morning! The current weather state is ' + Weather().description + ' and it is ' + str(Weather().temperature) + ' degrees Celsius.',
+            'NO_DATA': 'There is currently no data for your night.',
+            'TEMP_DATA': 'Here is your temperature data.',
+            'HUMID_DATA': 'Here is your humidity data',
+
+        }
