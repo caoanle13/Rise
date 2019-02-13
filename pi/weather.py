394fe86fb76eaa70ff88a355fe5d8645bc4f9365
Cao An <caoan.le15@imperial.ac.uk>
2019-02-13 15:59:37 +0000

    corrected weather api call

--- pi/weather.py
+++ pi/weather.py
@@ -5,15 +5,15 @@ import json
 class Weather:
 
  def __init__(self):
-    url = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22"
+    url = "https://api.worldweatheronline.com/premium/v1/weather.ashx?key=23fc338160134dc3aa4155139191302&q=London,united+kingdom&format=json"
     if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
             getattr(ssl, '_create_unverified_context', None)): 
             ssl._create_default_https_context = ssl._create_unverified_context
     with urllib.request.urlopen(url) as r:
         response = json.loads(r.read().decode())
 
-    self.description = response['weather'][0]['description']
-    self.temperature = int(response['main']['temp'] - 273.15)
+    self.description = response['data']['current_condition'][0]['weatherDesc'][0]['value']
+    self.temperature = int(response['data']['current_condition'][0]['temp_C'])
 
         
 if __name__ == '__main__':
